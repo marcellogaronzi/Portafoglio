52af55db8cb9c4ebfb2bf768e0378b7394380b8b
mxzzu <84173834+mxzzu@users.noreply.github.com>
2023-01-27 09:09:55 +0100

    Update csv_to_dict.py

--- scripts/csv_to_dict.py
+++ scripts/csv_to_dict.py
@@ -1,6 +1,12 @@
 import csv
 
 def csv_to_dict():
+    """
+    Convverts a .csv file into a key/value dictionary.
+    
+    Returns:
+    -data: dictionary
+    """   
     data = {}
     with open("out/result.csv") as csv_file:
         csv_reader = csv.DictReader(csv_file)
