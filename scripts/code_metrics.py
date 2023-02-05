701de417f17ace6e5a84212b2dcc6ff25fe24291
SerratoreFe <120744781+SerratoreFe@users.noreply.github.com>
2023-02-05 23:46:55 +0100

    Update code_metrics.py

--- scripts/code_metrics.py
+++ scripts/code_metrics.py
@@ -2,13 +2,13 @@ import lizard
 
 
 def calculate_code_metrics(filePath):
-    cocomo_dict = lizard.analyze_file(filePath)
+    code_metrics_dict = lizard.analyze_file(filePath)
 
-    result = cocomo_dict.__dict__
+    result = code_metrics_dict.__dict__
 
     function_list = []
     for i in range(len(result['function_list'])):
-        function_list.append(cocomo_dict.function_list[i].__dict__)
+        function_list.append(code_metrics_dict.function_list[i].__dict__)
 
     result['function_list'] = function_list
 
